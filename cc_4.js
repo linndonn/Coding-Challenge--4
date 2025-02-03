853723515d23978f25be3dc47a3b2023c662fd50
linndonn <linndonn@usf.edu>
2025-02-02 19:27:31 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -11,4 +11,17 @@ if (purchaseAmount > 100) { finalAmount = purchaseAmount * 0.85; }
 
 console.log(`Final amount after discount: $${finalAmount}`);
 
+    //Task 2 - For Loop
+    let sales = [ 100, 230, 55, 75, 120 ];
+    //Declare an array sales with the values
+    let totalSales = 0;
+    //Declare a variable totalSales and assign it the value of 0
+
+    for (let i = 0; i < sales.length; i++) { totalSales += sales[i];
+    }
+    //Use a for loop to calculate the total sales
+
+    console.log(`Total sales: $${totalSales}`);
+    //Log the total sales to the console using a template literal
+
     
